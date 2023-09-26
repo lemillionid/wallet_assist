0206b8327caafa3619607ee7dc3ac164a98c1cbe
Yoga Pratama <yoga@macs-MacBook-Pro.local>
2023-09-26 15:02:18 +0700

    rev bank_sycn

--- lib/src/features/banksycn/bank_sycn.dart
+++ lib/src/features/banksycn/bank_sycn.dart
@@ -1,31 +1,116 @@
 import 'package:flutter/material.dart';
 
-class BankSyncPage extends StatelessWidget {
+class BankSyncPage extends StatefulWidget {
+  const BankSyncPage({Key? key}) : super(key: key);
+
+  @override
+  _BankSyncPageState createState() => _BankSyncPageState();
+}
+
+class _BankSyncPageState extends State<BankSyncPage> {
+  String selectedCountry = 'Indonesia';
+  String selectedBank = 'Bank Indonesia';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Bank Sync'),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
             Text(
-              'Ini adalah halaman Bank Sync.',
+              'Search Your Bank',
               style: TextStyle(
-                fontSize: 24.0,
+                fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 16),
+            Icon(
+              Icons.account_balance,
+              size: 100,
+            ),
+            SizedBox(height: 25),
+            Text(
+              'Select Your Country',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            DropdownButton<String>(
+              value: selectedCountry,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedCountry = newValue!;
+                });
+              },
+              items: <String>['Indonesia', 'Other Country'].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              isExpanded: true,
+              underline: Container(
+                height: 2,
+                color: Colors.blue, // Warna garis bantu
+              ),
+            ),
+            SizedBox(height: 25),
+            Text(
+              'Select Your Bank',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            DropdownButton<String>(
+              value: selectedBank,
+              onChanged: (String? newValue) {
+                setState(() {
+                  selectedBank = newValue!;
+                });
+              },
+              items: <String>['Bank Indonesia', 'Other Bank'].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              isExpanded: true,
+              underline: Container(
+                height: 2,
+                color: Colors.blue, // Warna garis bantu
+              ),
+            ),
+            SizedBox(height: 32),
             ElevatedButton(
-              onPressed: () {},
-              child: Text('Sync Bank Data'),
+              onPressed: () {
+                // Tambahkan logika untuk tombol "Continue" di sini
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // Warna latar belakang hijau
+                onPrimary: Colors.white, // Warna teks putih
+              ),
+              child: Text(
+                'Continue',
+                style: TextStyle(fontSize: 18),
+              ),
             ),
           ],
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: BankSyncPage(),
+  ));
 }
